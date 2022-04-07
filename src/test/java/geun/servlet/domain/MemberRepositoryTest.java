6d044e72d75c4308037b87c8c4ed6a7d35488af3
Geunwoo Lee <player_geun@naver.com>
2022-04-07 13:36:06 +0900

    JSP #1 - web application requirement
    회원 도메인, 회원 저장소, 회원 저장소 테스트 코드

---
+++ src/test/java/geun/servlet/domain/MemberRepositoryTest.java
@@ -0,0 +1,51 @@
+package geun.servlet.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {      // 테스트는 실행 순서가 보장되지 않아서
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        // Given
+        Member member = new Member("hello", 20);
+
+        // When
+        Member savedMember = memberRepository.save(member);
+
+        // Then
+        Member findMember = memberRepository.findById(savedMember.getId());
+
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll() {
+        // Given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // When
+        List<Member> result = memberRepository.findAll();
+
+        // Then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
