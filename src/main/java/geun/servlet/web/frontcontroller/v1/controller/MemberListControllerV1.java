719c4013b9c455b82a51fd3740e6afb08f97e1de
Geunwoo Lee <player_geun@naver.com>
2022-04-14 17:14:26 +0900

    MVC Framework #1 - v1
    프론트 컨트롤러를 단계적으로 도입
    첫번째 버젼
    
    -인터페이스를 구현하여 다형성을 활용

---
+++ src/main/java/geun/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -0,0 +1,29 @@
+package geun.servlet.web.frontcontroller.v1.controller;
+
+import geun.servlet.domain.member.Member;
+import geun.servlet.domain.member.MemberRepository;
+import geun.servlet.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MvcMemberListServlet.service");
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
