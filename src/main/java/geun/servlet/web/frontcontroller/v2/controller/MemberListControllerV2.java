5940a1e6caa407b42a7c4958fc4f625f113a83ea
Geunwoo Lee <player_geun@naver.com>
2022-05-03 20:43:07 +0900

    MVC #2 - v2
    개선 사항
    1. view 분리
    모든 컨트롤러에서 뷰로 이동하는 부분에 중복 존재
    -> 이 부분을 MyView 객체를 통해 통합

---
+++ src/main/java/geun/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,25 @@
+package geun.servlet.web.frontcontroller.v2.controller;
+
+import geun.servlet.domain.member.Member;
+import geun.servlet.domain.member.MemberRepository;
+import geun.servlet.web.frontcontroller.MyView;
+import geun.servlet.web.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}
