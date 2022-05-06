960ca806a42075318b9c155cd830c1a8662102c1
Geunwoo Lee <player_geun@naver.com>
2022-05-06 13:19:41 +0900

    MVC #4 - v4
    개선사항
    1. 컨트롤러가 ModelView를 반환x, view의 논리적 이름만 반환

---
+++ src/main/java/geun/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,21 @@
+package geun.servlet.web.frontcontroller.v4.controller;
+
+import geun.servlet.domain.member.Member;
+import geun.servlet.domain.member.MemberRepository;
+import geun.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        List<Member> members = memberRepository.findAll();
+        model.put("members", members);
+
+        return "members";
+    }
+}
